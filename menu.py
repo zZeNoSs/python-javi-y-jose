f4383c5bacc559d8ddceb9d30b902cacd4d5332d
Josezg31 <joselitozg32@gmail.com>
2024-11-25 10:25:58 +0100

    implementacion de opciones en el menu.py

--- menu.py
+++ menu.py
@@ -1,3 +1,5 @@
+from operaciones import sumar, restar, multiplicar, dividir
+
 def mostrar_menu():
     print("1- Sumar")
     print("2- Restar")
@@ -6,3 +8,60 @@ def mostrar_menu():
     print("5- Salir")
     opcion = input("Elige una opción: ")
     return opcion
+
+def ejecutar_opcion(opcion):
+    if opcion == "1":
+        a = input("Introduce el primer valor: ")
+        b = input("Introduce el segundo valor: ")
+        try:
+            a = float(a)
+            b = float(b)
+            resultado = sumar(a, b)
+            print(f"Resultado: {resultado}")
+        except ValueError:
+            print("Error: Ambos valores deben ser int o float")
+    elif opcion == "2":
+        a = input("Introduce el primer valor: ")
+        b = input("Introduce el segundo valor: ")
+        try:
+            a = float(a)
+            b = float(b)
+            resultado = restar(a, b)
+            print(f"Resultado: {resultado}")
+        except ValueError:
+            print("Error: Ambos valores deben ser int o float")
+    elif opcion == "3":
+        a = input("Introduce el primer valor: ")
+        b = input("Introduce el segundo valor: ")
+        try:
+            a = float(a)
+            b = float(b)
+            resultado = multiplicar(a, b)
+            print(f"Resultado: {resultado}")
+        except ValueError:
+            print("Error: Ambos valores deben ser int o float")
+    elif opcion == "4":
+        a = input("Introduce el primer valor: ")
+        b = input("Introduce el segundo valor: ")
+        try:
+            a = float(a)
+            b = float(b)
+            resultado = dividir(a, b)
+            print(f"Resultado: {resultado}")
+        except ValueError:
+            print("Error: Ambos valores deben ser int o float")
+    elif opcion == "5":
+        print("Saliendo...")
+    else:
+        print("Opción no válida")
+
+if __name__ == "__main__":
+    while True:
+        opcion = mostrar_menu()
+        ejecutar_opcion(opcion)
+        if opcion == "5":
+            break
+
+
+
+
