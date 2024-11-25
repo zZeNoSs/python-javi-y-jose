62a593bb2aec7c89941b085e5409b42af684b02d
javierRodriguez <elzenyt906@gmail.com>
2024-11-25 10:17:17 +0100

    funcion menu y llamada de funcion en main

---
+++ main.py
@@ -0,0 +1,3 @@
+from menu import mostrar_menu 
+if __name__ == "__main__":
+    mostrar_menu()
---
+++ menu.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+    print("1- Sumar")
+    print("2- Restar")
+    print("3- Multiplicar")
+    print("4- Dividir")
+    print("5- Salir")
+    opcion = input("Elige una opción: ")
+    return opcion
